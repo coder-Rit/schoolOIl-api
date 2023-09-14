0d7673acc720681f2a806a6a7ebcc2f316b74270
coder-Rit <83896563+coder-Rit@users.noreply.github.com>
2023-09-14 21:23:34 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const catchAsyncErorr = require("./catchAsyncErorr")
 
 exports.isAuthenticated = catchAsyncErorr(async(req,res,next)=>{
 
-     
+     console.log(req.body)
     const token = req.body.token
     if (!token) {
         next(new ErrorHandler("Please login to access this source",400))
