e6118b364bfcd7ff4412b83290a3b1aed63af271
coder-Rit <83896563+coder-Rit@users.noreply.github.com>
2023-09-14 21:53:27 +0530

    Update registerClgRouter.js

--- router/registerClgRouter.js
+++ router/registerClgRouter.js
@@ -7,8 +7,8 @@ const { authorizedRole, isAuthenticated } = require('../middleware/auth')
 const Router = express.Router()
 
 Router.route("/registerClg").post(isAuthenticated,authorizedRole("admin"),registerClg)
-Router.route("/getAllRegisterClg").get(isAuthenticated,authorizedRole("admin"),getAllregisterdClg)
-Router.route("/collegeCode/:clgCode").get(isAuthenticated,getClg)
-Router.route("/clgShortName/:clgShortName").get(isAuthenticated,getClgByShortName)
+Router.route("/getAllRegisterClg").post(isAuthenticated,authorizedRole("admin"),getAllregisterdClg)
+Router.route("/collegeCode/:clgCode").post(isAuthenticated,getClg)
+Router.route("/clgShortName/:clgShortName").post(isAuthenticated,getClgByShortName)
  
 module.exports =Router   
