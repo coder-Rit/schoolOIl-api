357f2825eb5dbad09f63bc8eaf05974ac0f25799
coder-Rit <83896563+coder-Rit@users.noreply.github.com>
2023-09-14 21:51:54 +0530

    post

--- router/divisionRouter.js
+++ router/divisionRouter.js
@@ -7,10 +7,10 @@ const Router = express.Router()
 
 Router.route("/division/update").post(isAuthenticated,authorizedRole("teacher"), updateDivision) 
 Router.route("/divisions").post(isAuthenticated,getDivisions) 
-Router.route("/division/:id").get(isAuthenticated, getSingleDivision) 
+Router.route("/division/:id").post(isAuthenticated, getSingleDivision) 
 Router.route("/divisions/update/:id").put(isAuthenticated,authorizedRole("teacher"),updateDivisionById) 
 Router.route("/division/updateBydata").post(isAuthenticated,authorizedRole("teacher"),updateDivisionBydata) 
-Router.route("/division/myDivision/:div/:course/:clgShortName/:year").get(isAuthenticated,getDivisionByData) 
+Router.route("/division/myDivision/:div/:course/:clgShortName/:year").post(isAuthenticated,getDivisionByData) 
 Router.route("/division/enupdate").put(isAuthenticated,FindDivisionByIDAndUpdateEn) 
 Router.route("/division/delete/:id").delete(isAuthenticated,deleteDivision) 
    
