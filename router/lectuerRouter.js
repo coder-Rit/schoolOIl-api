e35bd99eb4d2916de2047f308db43c677dc28a4d
coder-Rit <83896563+coder-Rit@users.noreply.github.com>
2023-09-14 21:53:07 +0530

    Update lectuerRouter.js

--- router/lectuerRouter.js
+++ router/lectuerRouter.js
@@ -6,7 +6,7 @@ const { isAuthenticated } = require('../middleware/auth')
 const Router = express.Router()
 
 Router.route("/user/genrateL").post(isAuthenticated,genrateLecture)
-Router.route("/user/getAllLectures/:id/:from/:to").get(isAuthenticated,getAll_lectures)
+Router.route("/user/getAllLectures/:id/:from/:to").post(isAuthenticated,getAll_lectures)
 Router.route("/user/markMyAttendance/:updater/:update").post(isAuthenticated,update_lecture_for_attendance)
 Router.route("/user/lecture/:id").delete(isAuthenticated,delete_lecture)
 Router.route("/user/attendance/repace").put(isAuthenticated,findLectureByIDAndReplaceAttendance)
